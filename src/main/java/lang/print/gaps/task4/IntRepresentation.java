eba6e55012469da735cc44001b98f3c82293bc3c
Asset Kad <asset_kad@mail.ru>
2023-05-06 09:33:34 +0500

    finished stage 0, module #3, task#4

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -1,6 +1,10 @@
 package lang.print.gaps.task4;
 
 public class IntRepresentation {
-    public static void main(String[] args) {
-    }
+  public static void main(String[] args) {
+    char c = 99;
+    char h = 104;
+    char a = 97;
+    char r = 114;
+  }
 }
--- src/test/java/lang/print/gaps/task4/IntRepresentationTest.java
+++ src/test/java/lang/print/gaps/task4/IntRepresentationTest.java
@@ -14,18 +14,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class IntRepresentationTest extends BaseIOTest {
 
-    @Test
-    void mainPrintToConsoleExpected() throws IOException {
-        Path path = Paths.get("src/main/java/lang/print/gaps/task4/IntRepresentation.java");
-        List<String> strings = Files.readAllLines(path);
-        List<String> charC = strings.stream().filter(line -> line.contains("99")).collect(Collectors.toList());
-        List<String> charH = strings.stream().filter(line -> line.contains("104")).collect(Collectors.toList());
-        List<String> charA = strings.stream().filter(line -> line.contains("97")).collect(Collectors.toList());
-        List<String> charR = strings.stream().filter(line -> line.contains("114")).collect(Collectors.toList());
+  @Test
+  void mainPrintToConsoleExpected() throws IOException {
+    Path path = Paths.get("src/main/java/lang/print/gaps/task4/IntRepresentation.java");
+    List<String> strings = Files.readAllLines(path);
+    List<String> charC = strings.stream().filter(line -> line.contains("99")).collect(Collectors.toList());
+    List<String> charH = strings.stream().filter(line -> line.contains("104")).collect(Collectors.toList());
+    List<String> charA = strings.stream().filter(line -> line.contains("97")).collect(Collectors.toList());
+    List<String> charR = strings.stream().filter(line -> line.contains("114")).collect(Collectors.toList());
 
-        assertEquals(1, charC.size());
-        assertEquals(1, charH.size());
-        assertEquals(1, charA.size());
-        assertEquals(1, charR.size());
-    }
+    assertEquals(1, charC.size());
+    assertEquals(1, charH.size());
+    assertEquals(1, charA.size());
+    assertEquals(1, charR.size());
+  }
 }
